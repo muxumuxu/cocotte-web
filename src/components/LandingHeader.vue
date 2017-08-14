4324fb3216d39d1cc37f77f4006ebad37b110224
audreylbx <audreylabuxiere@gmail.com>
2017-08-14 12:41:41 +0200

    [+] Added landing header

---
+++ src/components/LandingHeader.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="landing-header">
+    <header>
+      <div class="logo">
+        <img src="../assets/images/logo.svg">
+        <span>cocotte</span>
+      </div>
+      <img src="../assets/images/app-store-badge.svg">
+    </header>
+    <h1>Quels aliments peut-on manger pendant la grossesse ?</h1>
+    <input type="search">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'landing-header',
+  computed: { },
+  components: { },
+  methods: { }
+}
+</script>
+
+<style>
+</style>
---
+++ src/components/LandingPage.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="landing-page">
+    <landing-header />
+  </div>
+</template>
+
+<script>
+import LandingHeader from '@/components/LandingHeader'
+
+export default {
+  computed: { },
+  components: { LandingHeader },
+  methods: {
+  }
+}
+</script>
+
+<style>
+</style>
