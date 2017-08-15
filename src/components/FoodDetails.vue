ef800658b1dfc642867471ba048800486a4daa34
audreylbx <audreylabuxiere@gmail.com>
2017-08-15 16:47:40 +0200

    [+] Created food details component

---
+++ src/components/FoodDetails.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="food-details">
+    <div class="wrapper">
+      <button>Ajouter en favoris</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'food-details',
+  computed: { },
+  components: { },
+  methods: { }
+}
+</script>
+
+<style>
+</style>
--- src/components/FoodPage.vue
+++ src/components/FoodPage.vue
@@ -1,16 +1,18 @@
 <template>
   <div id="food-page">
     <category-header :showFilters="false" />
+    <food-details />
     <banner-app />
   </div>
 </template>
 
 <script>
 import CategoryHeader from '@/components/CategoryHeader'
+import FoodDetails from '@/components/FoodDetails'
 import BannerApp from '@/components/BannerApp'
 export default {
   name: 'food-page',
-  components: { CategoryHeader, BannerApp },
+  components: { CategoryHeader, FoodDetails, BannerApp },
   methods: { }
 }
 </script>
