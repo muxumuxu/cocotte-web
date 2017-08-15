dffa116bb9e099dd3c6a0a0ad06ff759b4b3663c
audreylbx <audreylabuxiere@gmail.com>
2017-08-15 15:40:19 +0200

    [*] Updated food item

---
+++ src/components/FoodItem.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="food-item">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'food-item',
+  computed: { },
+  components: { },
+  methods: { }
+}
+</script>
+
+<style>
+</style>
