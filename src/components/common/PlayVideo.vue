a1131fd58fab5bf63d8374530473f0e518b97caf
audreylbx <audreylabuxiere@gmail.com>
2017-08-28 15:59:20 +0200

    [*] Updated js

--- src/components/common/PlayVideo.vue
+++ src/components/common/PlayVideo.vue
@@ -1,8 +1,9 @@
 <template>
   <div id="play-video">
-    <button id="play-button" v-on:click="playVideo"><img src="../../assets/images/pink-button.svg"></button>
+    <button v-on:click="playVideo"><img src="../../assets/images/pink-button.svg"></button>
     <p>{{$t('video')}}</p>
     <div id="video-overlay">
+      <button v-on:click="closeVideo"><span class="close"></span></button>
       <iframe src="https://player.vimeo.com/video/231192959?color=ff0079&title=0&byline=0&portrait=0" style="position:absolute;top:0;left:0;width:100%;height:100%;" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
     </div>
   </div>
@@ -20,13 +21,19 @@ export default {
       const player = new Player('handstick', {
         id: 231192959
       })
-      const button = document.getElementById('play-button')
       const video = document.getElementById('video-overlay')
 
-      button.click(function () {
-        video.classList.add('play')
-        player.api('play')
+      video.classList.add('play')
+      player.api('play')
+    },
+    closeVideo () {
+      const player = new Player('handstick', {
+        id: 231192959
       })
+      const video = document.getElementById('video-overlay')
+
+      video.classList.remove('play')
+      player.api('unload')
     }
   }
 }
@@ -60,6 +67,23 @@ export default {
     height: 100%;
     top: 0;
     left: 0;
+    display: none;
+    &.play {
+      display: block;
+    }
+    .close {
+      position: absolute;
+      top: 20px;
+      left: 20px;
+      color: white;
+      z-index: 10;
+      width: 40px;
+      height: 40px;
+      background: url(../../assets/images/close.svg) no-repeat 0 0;
+      cursor: pointer;
+      opacity: 0.6;
+      transition: .3s opacity linear;
+    }
   }
 }
 
